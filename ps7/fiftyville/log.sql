3a32cec6a458d0876fee677e6de4cbaae2491ac4
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 22:56:09 +0000

    Apr 6, 2024, 10:56 PM

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -2,12 +2,8 @@
 -- Find id and description for the CS50 duck report.
 SELECT id, description FROM crime_scene_reports WHERE month = 7 AND day = 28;
 
-
-
 SELECT id, transcript FROM interviews 
 WHERE month = 7 AND day = 28;
-
-
 
 SELECT name, transcript FROM interviews 
 WHERE id = 161 OR id = 162 OR id = 163;
