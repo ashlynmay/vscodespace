f1a940bfcc9d410696a5aa7f147c7bccdb5231b9
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 23:07:56 +0000

    Apr 6, 2024, 11:07 PM

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -10,14 +10,22 @@ WHERE month = 7 AND day = 28;
 SELECT name, transcript FROM interviews 
 WHERE id = 161 OR id = 162 OR id = 163;
 
--- Search for 
-SELECT license_plate FROM bakery_security_logs 
-WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity IS "exit";
-
-
+-- Investigate Ruth's claims that she saw the theif drive away 10 minutes after the theft and get names of people based off of license plate numbers..
 SELECT * FROM bakery_security_logs JOIN people ON people.license_plate = bakery_security_logs.license_plate 
 WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity IS "exit";
+-- Suspects: Vanessa, Bruce, Barry, Luca, Sofia, Iman, Diana, and Kelsey
 
-
+-- Investigate Eugene's claim that he saw the thief at the ATM on Leggett St the morning of the theft, and compare suspect with prior claims.
 SELECT person_id FROM bank_accounts JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number 
 WHERE month = 7 AND day = 28 AND atm_location IS "Leggett Street";
+-- Suspects: Bruce, Luca, Iman, and Diana
+
+-- Investigate Raymond's claim that he saw the theif call someone for less than 1 minute after the crime and compare to prior data.
+SELECT caller, receiver FROM phone_calls WHERE duration < 60 AND day = 28 AND month = 7;
+-- Callers: Sofia, Kelsey, Bruce, Kelsey, Diana
+-- Receivers: Luca
+-- Suspects: Bruce, Luca, and Diana
+
+-- Investigate Raymond's calim that the thief is taking the earliest flight out of Fiftyville the following morning.
+SELECT id FROM airports WHERE city IS "Fiftyville";
+SELECT 
