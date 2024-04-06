f179992293e12745df1075c83fac42551aa5c6df
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 01:02:17 +0000

    Apr 6, 2024, 1:02 AM

--- ps7/movies/8.sql
+++ ps7/movies/8.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN(SELECT id FROM movies WHERE title IS "Toy Story"));
+SELECT name FROM people WHERE id IN     (SELECT person_id FROM stars WHERE movie_id IN(SELECT id FROM movies WHERE title IS "Toy Story"));
