74a1b5cd1637e4fa37729a3df22952bb8bce18df
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 22:56:15 +0000

    Apr 6, 2024, 10:56 PM

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -2,22 +2,21 @@
 -- Find id and description for the CS50 duck report.
 SELECT id, description FROM crime_scene_reports WHERE month = 7 AND day = 28;
 
+
 SELECT id, transcript FROM interviews 
 WHERE month = 7 AND day = 28;
 
+
 SELECT name, transcript FROM interviews 
 WHERE id = 161 OR id = 162 OR id = 163;
-
 
 
 SELECT license_plate FROM bakery_security_logs 
 WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity IS "exit";
 
 
-
 SELECT * FROM bakery_security_logs JOIN people ON people.license_plate = bakery_security_logs.license_plate 
 WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity IS "exit";
-
 
 
 SELECT person_id FROM bank_accounts JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number 
