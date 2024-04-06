1d5454955ecd7017e4eb34a9c1a5b059bc55111e
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 01:37:55 +0000

    Apr 6, 2024, 1:37 AM

--- ps7/movies/12.sql
+++ ps7/movies/12.sql
@@ -1,5 +1,6 @@
 SELECT title FROM movies 
 WHERE id IN
-(SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name IS "Jennifer Lawrence")
+(SELECT movie_id FROM stars WHERE person_id IN 
+(SELECT id FROM people WHERE name IS "Jennifer Lawrence")
 AND  id IN (SELECT movie_id FROM stars WHERE person_id IN
 (SELECT id FROM people WHERE name IS "Bradley Cooper")));
