cbd6f713df761f44f922be987675a0fc85cc9922
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 01:24:17 +0000

    Apr 6, 2024, 1:24 AM

--- ps7/movies/11.sql
+++ ps7/movies/11.sql
@@ -1,4 +1,4 @@
-SELECT title FROM movies WHERE id IN
+SELECT title FROM movies JOIN ratings on movies.id = ratings.movie_id WHERE id IN
 (SELECT movie_id FROM stars WHERE person_id IN
 (SELECT id FROM people WHERE name IS "Chadwick Boseman"))
  LIMIT 5;
