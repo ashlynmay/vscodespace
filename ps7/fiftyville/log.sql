197a0d4118a0b3134f9dfdb0cb3e9a826f92d347
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 23:38:49 +0000

    Apr 6, 2024, 11:38 PM

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -2,18 +2,22 @@
 -- Find id and description for the CS50 duck report.
 SELECT id, description FROM crime_scene_reports WHERE month = 7 AND day = 28;
 
+
 -- Get interview transcripts and id's
 SELECT id, transcript FROM interviews 
 WHERE month = 7 AND day = 28;
+
 
 -- Whittle down transcripts that mention the bakery and get names of people.
 SELECT name, transcript FROM interviews 
 WHERE id = 161 OR id = 162 OR id = 163;
 
+
 -- Investigate Ruth's claims that she saw the theif drive away 10 minutes after the theft and get names of people based off of license plate numbers..
 SELECT * FROM bakery_security_logs JOIN people ON people.license_plate = bakery_security_logs.license_plate 
 WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity IS "exit";
 -- Suspects: Vanessa, Bruce, Barry, Luca, Sofia, Iman, Diana, and Kelsey
+
 
 -- Investigate Eugene's claim that he saw the thief at the ATM on Leggett St the morning of the theft, and compare suspect with prior claims.
 SELECT person_id FROM bank_accounts JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number 
