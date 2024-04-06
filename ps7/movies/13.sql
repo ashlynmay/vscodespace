97d18860be6219d34542b6a7fe7be1859b818619
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 01:46:38 +0000

    Apr 6, 2024, 1:46 AM

--- ps7/movies/13.sql
+++ ps7/movies/13.sql
@@ -2,5 +2,5 @@ SELECT name FROM people WHERE
 id IN
 (SELECT person_id FROM stars WHERE 
 movie_id = (SELECT movie_id FROM stars WHERE 
-person id = (SELECT id FROM people WHERE name IS "Kevin Bacon")
+person_id = (SELECT id FROM people WHERE name IS "Kevin Bacon")
 ));
