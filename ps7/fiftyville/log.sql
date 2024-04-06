a5629388b1933984baf6f5cce678385d03c43292
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 23:13:26 +0000

    Apr 6, 2024, 11:13 PM

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -23,7 +23,7 @@ WHERE month = 7 AND day = 28 AND atm_location IS "Leggett Street";
 -- Investigate Raymond's claim that he saw the theif call someone for less than 1 minute after the crime and compare to prior data.
 SELECT name FROM people JOIN phone_calls ON people.phone_number = phone_calls.caller WHERE duration < 60 AND day = 28 AND month = 7;
 -- Callers at Bakery: Sofia, Kelsey, Bruce, Kelsey, Diana
-SELECT name FROM people JOIN phone_calls ON people.phone_number = phone_calls.caller WHERE duration < 60 AND day = 28 AND month = 7;
+SELECT name FROM people JOIN phone_calls ON people.phone_number = phone_calls.receiver WHERE duration < 60 AND day = 28 AND month = 7;
 -- Receivers at Bakery: Luca
 -- Suspects: Bruce, Luca, and Diana
 
