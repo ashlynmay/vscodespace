59696fbb5d944f0867126430829097d14d98e421
ipsum <may.ashlyn0723@gmail.com>
2024-04-06 22:38:15 +0000

    Apr 6, 2024, 10:38 PM

--- ps7/fiftyville/log.sql
+++ ps7/fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
--- Find 
+-- Find id and description for the CS50 duck report.
 SELECT id, description FROM crime_scene_reports WHERE month = 7 AND day = 28;
 
