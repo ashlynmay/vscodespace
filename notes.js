abe35a91ae34a38b02f30fe752284db542dd1e18
Ashlyn May <59678757+ashlynmay@users.noreply.github.com>
2024-04-12 22:08:45 -0500

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,9 @@
+similar to c Syntax 
+runs client side
+variables are similar to python, with no type selector.
+var variable = data; 
+conditionals: lseif, else, switch, ?: are included
+
+
+<script>include js in html</script>
+<link> rel="script" href="script.js"</link>
