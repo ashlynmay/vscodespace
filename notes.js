da4d249abfa16855332c5a1cc2b2ccf0aaa32517
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 21:10:41 +0000

    Apr 13, 2024, 9:10 PM

--- notes.js
+++ notes.js
@@ -10,7 +10,8 @@ conditionals:
     if, elseif, else, switch, ?: are included
 loops: 
     while, do-while, for
-functions: function name() {}, some functions can be anonymous (no name)
+functions: 
+    function name() {}, some functions can be anonymous (no name)
 arrays: var nums = [1, 2, 3, 4, 5];
         var mixed = [1, true, 3.333, 'five'];
 
