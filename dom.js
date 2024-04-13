1507935a4a52f2d90edc6d0b4ca4f91c3f167fe9
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 22:23:08 +0000

    Apr 13, 2024, 10:23 PM

--- dom.js
+++ dom.js
@@ -58,7 +58,9 @@ change color of element to purple:
 
     jQuery:
         html: <button class="jQButton">Purple</button>
-        script: $(document).ready(function()
+        script: $(document).ready(function() {
+                    $('.jQButton')
+        }
                 )
 
 */
