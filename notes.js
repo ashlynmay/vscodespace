c4f55f8be8e04b5307375d1940178132a39c9239
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 19:42:50 +0000

    Apr 13, 2024, 7:42 PM

--- notes.js
+++ notes.js
@@ -11,7 +11,7 @@ objects/structs: instead of function(object); do object.function();
         var herbie = {year : 1963, model: 'Beetle'};
 
 loops: 
-    
+    var wkArray = ['M']
 
 
     for (var key in object)
