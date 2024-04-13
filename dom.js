7bcb094c2b1e5d5e644163d73a090a28f5765fe4
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 21:27:34 +0000

    Apr 13, 2024, 9:27 PM

---
+++ dom.js
@@ -0,0 +1,6 @@
+/*
+dom stands for document object model
+javascript objects allow you to nest other objects inside them.
+the document object allows u to organize the entire content of a webpage as an object
+this allows you to manipulate the pages elements programmatically
+*/
