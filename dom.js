a685014dbf9564098670253cf0425b6197694847
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 22:18:42 +0000

    Apr 13, 2024, 10:18 PM

--- dom.js
+++ dom.js
@@ -51,7 +51,7 @@ change color of element to purple:
 
     event handler:
         html: <button onclick="changeColorEvent(event);">Purple</button>
-        script:
+        script: 
 
     jQuery:
         html: <button class="jQButton">Purple</button>
