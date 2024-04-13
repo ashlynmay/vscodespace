273527224b24e23bd549b78b2d5ac77435df8b2a
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 22:24:02 +0000

    Apr 13, 2024, 10:24 PM

--- dom.js
+++ dom.js
@@ -60,7 +60,7 @@ change color of element to purple:
         html: <button class="jQButton">Purple</button>
         script: $(document).ready(function() {
                     $('.jQButton').click(function() {
-                        $('#colorDiv').css('background-color',)
+                        $('#colorDiv').css('background-color', this.innerHTML.toLowerCase());
                     })
         }
                 )
