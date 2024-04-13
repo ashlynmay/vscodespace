ea7933c316ef189503cd4dfdfdfd3bfb4d4e3383
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 22:18:00 +0000

    Apr 13, 2024, 10:18 PM

--- dom.js
+++ dom.js
@@ -43,7 +43,7 @@ jquery:
 change color of element to purple:
     in js with 1 function per color:
         html: <button onclick="turnPurple();">Purple</button>
-        script: function turnPurpledocument.getElementById('colorDiv').style.backgroundColor = 'purple';
+        script: function turnPurple() {document.getElementById('colorDiv').style.backgroundColor = 'purple';
 
     in js with 1 function for all colors with a parameter:
         html: <button onclick="changeColor('purple');">Purple</button>
