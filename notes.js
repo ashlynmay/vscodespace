d2cca0f77362cd34029cf72cf9261a003e69dceb
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 19:32:50 +0000

    Apr 13, 2024, 7:32 PM

--- notes.js
+++ notes.js
@@ -6,7 +6,7 @@ conditionals: if, elseif, else, switch, ?: are included
 loops: while, do-while, for
 functions: function name() {}, some functions can be anonymous (no name)
 arrays: var nums = [1, 2, 3, 4, 5]
-        var 
+        var mixed = [1, ]
 
 <script>include js in html</script>
 <link> rel="script" href="script.js"</link>
