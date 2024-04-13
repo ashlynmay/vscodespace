57e2a7f066cc2d42bc22503a1ddcc584100056dd
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 21:10:15 +0000

    Apr 13, 2024, 9:10 PM

--- notes.js
+++ notes.js
@@ -1,6 +1,11 @@
 /* similar to c Syntax 
 runs client side, variables are similar to python with no type selector, can behave like an object-oriented language
+
+<script>include js in html</script>
+<link> rel="script" href="script.js"</link>
+
 var variable = data; 
+
 conditionals: if, elseif, else, switch, ?: are included
 loops: while, do-while, for
 functions: function name() {}, some functions can be anonymous (no name)
