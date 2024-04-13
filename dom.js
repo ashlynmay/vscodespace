473abb4772f3e5bf38d0ecc43bdb57a64c2b3bd0
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 21:57:55 +0000

    Apr 13, 2024, 9:57 PM

--- dom.js
+++ dom.js
@@ -25,5 +25,6 @@ major dom properties:
     id: id attribute of an HTML element. <p id="greeting"> would be greeting
     parentNode: reference to node one level up: <title> would return <head>
     childNodes: array of references to the nodes on level down: <body> would return [h2, p, a]
-    attributes: array of attributes of an html elements:
+    attributes: array of attributes of an html elements
+    style: object containing th
 */
