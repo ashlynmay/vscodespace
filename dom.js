f768d4c0b33070ecf2048ca3c33d80753cc7f369
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 21:28:23 +0000

    Apr 13, 2024, 9:28 PM

--- dom.js
+++ dom.js
@@ -4,5 +4,6 @@ javascript objects allow you to nest other objects inside them.
 the document object allows u to organize the entire content of a webpage as an object
 this allows you to manipulate the pages elements programmatically
 
-
+example html:
+    
 */
