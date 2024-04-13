92a967498015f2f3e8e537a8e31dbf549db2bd09
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 21:50:09 +0000

    Apr 13, 2024, 9:50 PM

--- dom.js
+++ dom.js
@@ -20,5 +20,6 @@ see DOM:
     open developer console and run console.dir(document), and check children 
 
 major dom properties:
-    innerHTML: html inside of tags. ex: <title> would be Hello,world
+    innerHTML: html inside of tags. ex: <title> would be Hello, world
+    nodeName: 
 */
