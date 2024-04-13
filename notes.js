61b13faf78641d437312639732438d13fc5e899b
ipsum <may.ashlyn0723@gmail.com>
2024-04-13 21:12:42 +0000

    Apr 13, 2024, 9:12 PM

--- notes.js
+++ notes.js
@@ -66,7 +66,7 @@ events:
     an event in HTML and JavaScript is a response to user interaction with the web page.w
     JavaScript has support for event handlers, which are callback functions that respond to HTML events.
 
-    event in html: <button onclick="alertName(event)">Button 1</button>
+event in html: <button onclick="alertName(event)">Button 1</button>
 
     function alertName(event)
     {
