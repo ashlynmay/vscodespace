6b9e8ca7d4851444fb2d7c529f18798ec785c511
ipsum <may.ashlyn0723@gmail.com>
2024-04-17 03:51:05 +0000

    Apr 16, 2024, 10:51 PM

--- notes/flask.py
+++ notes/flask.py
@@ -50,7 +50,7 @@ change "name = request.args["name"] to:
 use html form to change link values:
     <form method ="get">
         <input autocomplete=off autofocus name="name" placeholder="Name" type="text">
-        <button type="submit">Greet
+        <button type="submit">Greet<button>
 
     
 """
