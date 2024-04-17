98a155b7c160e9f359a1459df24daa13f8845b36
ipsum <may.ashlyn0723@gmail.com>
2024-04-17 05:22:17 +0000

    Apr 17, 2024, 12:22 AM

--- notes/flask.py
+++ notes/flask.py
@@ -103,7 +103,7 @@ if u want to make a single route support multiple meethods:
             return render_template("index.html")
     
     in html:
-        
+        <form method="post">
     
     
 """
