0b99bfd90a41ed929345408999a192f445505704
ipsum <may.ashlyn0723@gmail.com>
2024-04-18 21:50:17 +0000

    Apr 18, 2024, 4:50 PM

--- notes/flask.py
+++ notes/flask.py
@@ -107,5 +107,5 @@ if u want to make a single route support multiple meethods:
             code
         </form>
     
-clicking greet without any text 
+clicking greet without any text will not use the 
 """
