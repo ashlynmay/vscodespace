97322ee727196962019dace7d5a835db8a30314e
ipsum <may.ashlyn0723@gmail.com>
2024-04-18 23:53:49 +0000

    Apr 18, 2024, 6:53 PM

--- notes/flask.py
+++ notes/flask.py
@@ -117,7 +117,7 @@ clicking greet without any text will not use the "world" placeholder, as it is s
 
 html dropdown:
     <select name="name">
-        <option disabled selected value="">world</option>
+        <option disabled selected value="">Name</option>
         <option value="Ashlyn">Ashlyn</option>
         <option value="Ashley">Ashley</option>
         <option value="Ashlynn">Ashlynn</option>
