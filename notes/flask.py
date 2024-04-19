634bfb82b2d3fbd06cddb83161faff15554a2c90
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 02:09:20 +0000

    Apr 18, 2024, 9:09 PM

--- notes/flask.py
+++ notes/flask.py
@@ -266,6 +266,8 @@ cookies ?
         @app.route("/")
         def index():
             return render_template("index.html")
+            
+        @app.route()
     
     in index.html:
         {% extends "layout.html" %}
