b731df6b47a2f0af08edc30c9f6cbf26fb05f154
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 01:09:19 +0000

    Apr 18, 2024, 8:09 PM

--- notes/flask.py
+++ notes/flask.py
@@ -162,7 +162,7 @@ making an error page:
 in error.html:
     {% extends "layout.html" %}
     {% block body %}
-        <p></p>
+        <p>{{ error }}</p>
         <p>something went wrong</p>
     {% endblock %}
 """
