6426489f777fc1c655b54b621deeb0409bcd2d70
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 00:28:27 +0000

    Apr 18, 2024, 7:28 PM

--- notes/flask.py
+++ notes/flask.py
@@ -149,5 +149,5 @@ how do i make sure its one of the names i have in html?
             return render_template("failure.html")
         return render_template("success.html")
     
-
+what if im using checkboxes and what all values selected?
 """
