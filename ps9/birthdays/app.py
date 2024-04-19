e6619253a75d4afbe28e9f9cc624eff9e0364e34
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 23:10:49 +0000

    Invalid DateTime

--- ps9/birthdays/app.py
+++ ps9/birthdays/app.py
@@ -30,7 +30,7 @@ def index():
         request.form.get("name")
         request.form.get("month")
         request.form.get("day")
-        INSERT INTO birthdays
+        db.INSERT INTO birthdays
         
         return redirect("/")
 
