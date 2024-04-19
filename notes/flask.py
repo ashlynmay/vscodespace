3fd849e16edbe3fa77c882e9798c45edf211143b
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 01:36:56 +0000

    Apr 18, 2024, 8:36 PM

--- notes/flask.py
+++ notes/flask.py
@@ -222,7 +222,7 @@ how do i ensure that the information will survive after reboot or server restart
     
     in registrants.html:
         {% extends "layout.html" %}
-        
+        {% block body %}
         {% for registrant in registrants %}
             <li>{{ registrant["name"] }}: {{ registrant["value"] }}</li>
         {% endfor %}
