7f60ff9f1f4ced2d3a008a0aa312e2691162b49e
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 00:28:56 +0000

    Apr 18, 2024, 7:28 PM

--- notes/flask.py
+++ notes/flask.py
@@ -153,5 +153,5 @@ what if im using checkboxes and what all values selected?:
 in app.py:
     if not request.form.get("name"):
         return render_template("failure.html")
-        
+    for sport in 
 """
