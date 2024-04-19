7ced6c2812448f0ff318dcedb371d8d9cd7b7501
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 23:14:02 +0000

    Invalid DateTime

--- ps9/birthdays/app.py
+++ ps9/birthdays/app.py
@@ -27,9 +27,6 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        request.form.get("name")
-        request.form.get("month")
-        request.form.get("day")
         db.execute("INSERT INTO birthdays (name, month, day) VALUES (request.form.get('name'), request.form.get('month'), request.form.get('day'))")
         
         return redirect("/")
