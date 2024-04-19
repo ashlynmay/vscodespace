13defabc3fb416346a89d9ad681a92da9ec1a9fe
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 01:27:36 +0000

    Apr 18, 2024, 8:27 PM

--- notes/flask.py
+++ notes/flask.py
@@ -182,6 +182,8 @@ how do i actually register the a value to a name in app.py?
         @app.route("/register", methods=["POST"])
         def register():
             ... code to definine register
-            REGISTRANTS[name] = value
+        REGISTRANTS[name] = value
+        
+            
 
 """
