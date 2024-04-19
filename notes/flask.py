93ba90346c11a5a4a571d64d8dd09736a67e53d3
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 02:08:01 +0000

    Apr 18, 2024, 9:08 PM

--- notes/flask.py
+++ notes/flask.py
@@ -270,7 +270,7 @@ cookies ?
     in index.html:
         {% extends "layout.html" %}
         {% block body %}
-            <h1>Hello, {{ session["name"] }}!</h1>
+            {}
         {% endblock %}
 
 """
