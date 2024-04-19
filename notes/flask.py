f36c5317afa53f83de85aa3699f066978cb6ba32
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 01:12:52 +0000

    Apr 18, 2024, 8:12 PM

--- notes/flask.py
+++ notes/flask.py
@@ -169,7 +169,7 @@ in error.html:
     
 in app.py:
     if not request.form.get("name"):
-        return render_template("failure.html")
+        return render_template("error.html")
     for name in request.form.getlist("name"):
         if name not in NAMES:
             return render_template("failure.html")
