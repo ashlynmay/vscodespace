dc19aef4c07323a88ea12ec578eb24d6fd2bece4
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 23:08:27 +0000

    Invalid DateTime

--- ps9/birthdays/app.py
+++ ps9/birthdays/app.py
@@ -27,7 +27,7 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        request.
+        request.form
         
         return redirect("/")
 
