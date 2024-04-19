9c697876c1e295f5f9999bdea90aad7e6e0f1588
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 01:36:10 +0000

    Apr 18, 2024, 8:36 PM

--- notes/flask.py
+++ notes/flask.py
@@ -187,38 +187,38 @@ how do i actually register the a value to a name in app.py?
         return redirect("https://google.com")
         
 how do i ensure that the information will survive after reboot or server restart?:
-in app.py:
-    from cs50 import SQL
-    from flask import Flask, redirect, render_template, request
-    app = Flask(__name__)
-    db = SQL("sqlite:///database.db")
-    
-    NAMES = ["Ashlyn", "Ashley", "Ashlynn"]
-    
-    @app.route("/")
-    def index():
-        return render_template("index.html", names=NAMES)
-    
-    @app.route("/deregister", methods=["POST"])
-    def deregister():
-        id = request.form.get("id")
-        if id:
-            delete = db.execute("DELETE FROM registrants WHERE id = ?", id)
-        return redirect("/registrants")
-    
-    @app.route("/register", methods=["POST"])
-    def register():
-        name = request.form.get("name") 
-        value = request.form.get("value")
-        if not value or not name in NAMES:
-            return render_template("failure.html")   
-        db.execute("INSERT INTO registrants (name, value) VALUES (?, ?)", name, value)
-        return redirect("/registrants")
-    
-    @app.route("/registrants")
-    def registrants():
-        registrants = db.execute("SELECT * FROM registrants")
-        return render_template("registrants.html", registrants=registrants)
+    in app.py:
+        from cs50 import SQL
+        from flask import Flask, redirect, render_template, request
+        app = Flask(__name__)
+        db = SQL("sqlite:///database.db")
+        
+        NAMES = ["Ashlyn", "Ashley", "Ashlynn"]
+        
+        @app.route("/")
+        def index():
+            return render_template("index.html", names=NAMES)
+        
+        @app.route("/deregister", methods=["POST"])
+        def deregister():
+            id = request.form.get("id")
+            if id:
+                delete = db.execute("DELETE FROM registrants WHERE id = ?", id)
+            return redirect("/registrants")
+        
+        @app.route("/register", methods=["POST"])
+        def register():
+            name = request.form.get("name") 
+            value = request.form.get("value")
+            if not value or not name in NAMES:
+                return render_template("failure.html")   
+            db.execute("INSERT INTO registrants (name, value) VALUES (?, ?)", name, value)
+            return redirect("/registrants")
+        
+        @app.route("/registrants")
+        def registrants():
+            registrants = db.execute("SELECT * FROM registrants")
+            return render_template("registrants.html", registrants=registrants)
     
         
             
