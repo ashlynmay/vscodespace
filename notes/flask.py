d461dbebf99823a68009cd433b6565e60429d875
ipsum <may.ashlyn0723@gmail.com>
2024-04-19 02:06:11 +0000

    Apr 18, 2024, 9:06 PM

--- notes/flask.py
+++ notes/flask.py
@@ -261,6 +261,7 @@ cookies ?
     app = Flask(__name__)
     app.condig["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
     
     @app.route("/")
     def index():
