40a22c40f853bf026da6d6577ca17fdba1195cd3
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 21:43:30 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -234,7 +234,7 @@ def sell():
         else:
             shares = 0
         updated_shares = shares - int(request.form.get("shares"))
-        total_result = db.execute("SELECT total FROM purchases WHERE id = ? AND stock = ?", session["user_id"], request.form.get("symbol"))
+        total_result = db.execute("SELECT total FROM purchases WHERE id = ? AND stock = ?", session["user_id"], stock)
         total = total_result[0]["total"]
         price = lookup(request.form.get("symbol"))["price"]
         ntotal = int(request.form.get("shares")) * price
