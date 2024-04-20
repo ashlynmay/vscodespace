47b21b1e5932d92b6007715f807a3b99a25898ee
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 04:17:11 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -58,7 +58,7 @@ def buy():
             if cash-(price*shares) > 0:
                 payment successful
             else:
-                return apology("not enough ", 402)
+                return apology("not enough funds ", 402)
             
     
     # User reached route via GET (as by clicking a link or via redirect)
