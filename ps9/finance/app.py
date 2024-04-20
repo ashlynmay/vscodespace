7ec56e28bf08155887c6f235e087b2556c31a802
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 22:10:32 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -41,7 +41,7 @@ def index():
     cash_result = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
     cash = cash_result[0]["cash"] if cash_result else 0
 
-    total = cash + sum((purchased["0"])
+    total = cash + sum(purchased["0"])
     prices = []
     for entry in stock:
         stock_price = lookup(entry["stock"])["price"]
