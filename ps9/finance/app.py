db573f7c551142fe5121ca5faff3f4e6b9c25052
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 04:20:30 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -56,7 +56,8 @@ def buy():
             cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
             price = (lookup(request.form.get("symbol"))["price"])
             if cash-(price*shares) > 0:
-                if db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='purchases'")
+                if db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='purchases'"):
+                    
             else:
                 return apology("not enough funds to complete purchase", 402)
             
