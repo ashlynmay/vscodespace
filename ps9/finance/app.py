00e0c5a8ac9bae7620d609b9adc143c287ab084f
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 21:57:55 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -84,7 +84,7 @@ def buy():
 
                 existing_shares = db.execute("SELECT shares FROM purchases WHERE stock = ? AND id = ?", stock, session["user_id"])
                 existing_total = db.execute("SELECT total FROM purchases WHERE stock = ? AND id = ?", stock, session["user_id"])
-                existing_total = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
+                existing_cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
                 if existing_shares:
                     existing_shares_int = existing_shares[0]["shares"]
                     updated_shares = existing_shares_int + int(shares)  # Convert shares to an integer
