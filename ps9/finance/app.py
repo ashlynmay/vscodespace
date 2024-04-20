faf92f7588fab72cb926b5153907ca0a0f507e50
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 08:12:31 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -223,7 +223,7 @@ def sell():
         elif lookup(request.form.get("symbol")) == None:
             return apology("stock not found", 402)
         
-        shares = db.execute("SELECT shares FROM purchases WHERE id = ? AND ")
+        shares = db.execute("SELECT shares FROM purchases WHERE id = ? AND stock IS ?")
         if request.form.get("shares") < 1:
             return apology("you cant sell less than 1 share")
         elif request.form.get("shares") >  
