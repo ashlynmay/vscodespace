f929722bba09bc3b553eb7fca3bd10e255dc36d3
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 05:13:53 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -65,7 +65,7 @@ def buy():
                     if db.execute("SELECT shares FROM purchases WHERE stock IS ? AND id = ?", stock, session["user_id"]):
                         db.execute("INSERT INTO purchases (id, stock, shares, price, total) VALUES (?, ?, ?, ?, ?)", session["user_id"], stock, shares, price, total)
                     else:
-                        shares_result = db.execute
+                        shares_result = db.execute("")
                         db.execute("UPDATE purchases SET shares = ? WHERE stock IS ? AND id = ?", shares, stock, session["user_id"])
                 return redirect("/")
             else:
