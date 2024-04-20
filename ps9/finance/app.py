f685967d28d353b53c8cbaf74a31149902677c2e
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 21:23:21 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -45,7 +45,6 @@ def index():
     for entry in stock:
         stock_price = lookup(entry["stock"])["price"]
         prices.append(stock_price) 
-        total += total_value
         shares_for_stock = next((share["shares"] for share in shares if share["stock"] == entry["stock"]), 0)
         total_value = stock_price * shares_for_stock["shares"]
     combined_data = [{"stock": entry["stock"], "shares": shares_for_stock["shares"], "price": stock_price, "total_value": stock_price * shares_for_stock["shares"]} for entry, shares_for_stock, stock_price in zip(stock, shares, prices)]
