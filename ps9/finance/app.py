19080458e5713227013354193e5f2192da2c5d45
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 05:49:12 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -47,7 +47,7 @@ def index():
         shares_for_stock = next((shares["shares"] for shares in shares if shares["stock"] == entry["stock"]), 0)
         total += shares_for_stock * stock_price
     
-    return render_template("index.html", stock = stock, stotal=total)
+    return render_template("index.html", stock = stock, shares = shares, total=total)
 
 
 @app.route("/buy", methods=["GET", "POST"])
