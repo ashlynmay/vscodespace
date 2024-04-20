22d34b3fa3c276f8e990a1a9f4effcc573b7d771
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 04:29:48 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -58,7 +58,7 @@ def buy():
             shares = (request.form.get("shares"))
             stock = (request.form.get("symbol"))
             total = price*shares
-            if cash-(price*shares) > 0:
+            if cash-(total) > 0:
                 if db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='purchases'"):
                     db.execute("INSERT INTO purchases (id, stock, shares, price, total) VALUES (?, ?, ?, ?, ?)", session["user_id"], stock, shares, price, )
                 else:
