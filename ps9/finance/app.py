d2f31dd6adfc9a19833d9f9427ac4935230e1ac9
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 05:25:30 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -38,7 +38,7 @@ def index():
     if request.method == "POST":
         
     if request.method == "POST":
-    return apology("TODO")
+        return apology("TODO")
 
 
 @app.route("/buy", methods=["GET", "POST"])
