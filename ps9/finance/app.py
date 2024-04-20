43d197fdbcc3ef64b2a0a6817ac5e5b37c5f3af5
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 05:09:08 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -62,7 +62,7 @@ def buy():
                     db.execute("INSERT INTO purchases (id, stock, shares, price, total) VALUES (?, ?, ?, ?, ?)", session["user_id"], stock, shares, price, total)                
                 else:
                     db.execute("CREATE TABLE purchases (id INTEGER NOT NULL, stock TEXT NOT NULL, shares INTEGER NOT NULL, price NUMERIC NOT NULL, total NUMERIC NOT NULL)")
-                    if db.execute("SELECT shares FROM purchases WHERE stock IS ? AND id", stock):
+                    if db.execute("SELECT shares FROM purchases WHERE stock IS ? AND id = ?", stock, ):
                         db.execute("INSERT INTO purchases (id, stock, shares, price, total) VALUES (?, ?, ?, ?, ?)", session["user_id"], stock, shares, price, total)
                     else:
                         db.execute("INSERT INTO purchases (shares)")
