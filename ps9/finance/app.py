e37cca39a80741ebd1860998710634c5c1e2cd20
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 05:45:55 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -42,7 +42,7 @@ def index():
     cash = cash_result[0]["cash"] if cash_result else 0
     for entry in stock:
         price["entry"] = lookup("stock")["price"]
-    total = (sum(price)+
+    total = (sum(price) + cash
     return render_template("index.html", purchases = purchases)
 
 
