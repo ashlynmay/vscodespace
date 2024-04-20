a3bd1c98a6681905244d23b678ee463506909a13
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 05:08:52 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -65,7 +65,7 @@ def buy():
                     if db.execute("SELECT shares FROM purchases WHERE stock IS ?", stock):
                         db.execute("INSERT INTO purchases (id, stock, shares, price, total) VALUES (?, ?, ?, ?, ?)", session["user_id"], stock, shares, price, total)
                     else:
-                        db.execute("INSERT INTO purchases ()")
+                        db.execute("INSERT INTO purchases (shares)")
                 return redirect("/")
             else:
                 return apology("not enough funds to complete purchase", 402)
