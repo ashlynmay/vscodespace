7f4314ef9ea41c593acf071906a029071d4c1fb7
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 03:16:24 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -109,6 +109,8 @@ def quote():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         return 1
+    
+    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
     return apology("TODO")
