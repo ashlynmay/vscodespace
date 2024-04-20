d6687e1710751c6ac1018bfd5b0ee4dc9562af4a
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 03:36:16 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -111,7 +111,7 @@ def quote():
         if lookup(request.form.get("symbol")) != None:
             
         else:
-            return apology("")
+            return apology("stock not found")
     
     # User reached route via GET (as by clicking a link or via redirect)
     else:
