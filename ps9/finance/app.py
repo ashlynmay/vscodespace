0925077e3c32c1dc4d861e6f7da9b498c25bc0f8
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 04:11:59 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -53,7 +53,7 @@ def buy():
         elif request.form.get("shares") < 1:
             return apology("you cannot purchase less than 1 share", 402)
         else:
-            cash = db.execute("SELECT cash FROM users WHERE id = ?", )
+            cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
             
     
     # User reached route via GET (as by clicking a link or via redirect)
