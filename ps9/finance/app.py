c7e9ca9c4ebdbd0e05acb3bf10802e51350df074
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 04:06:23 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -53,7 +53,7 @@ def buy():
     
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("quote.html")
+        return render_template("buy.html")
     return apology("TODO")
 
 
