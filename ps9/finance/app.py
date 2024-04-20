52ac54297d08e3a7b32a0e04de76b0636f6d1363
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 04:23:15 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -59,7 +59,7 @@ def buy():
                 if db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='purchases'"):
                     db.execute("INSERT INTO purchases (id, stock, shares, price, total)")
                 else:
-                    db.execute("CREATE TABLE pu")
+                    db.execute("CREATE TABLE purchases ")
             else:
                 return apology("not enough funds to complete purchase", 402)
             
