d541256c9674053b6b6a5858a3802f82065638a7
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 21:45:29 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -92,7 +92,7 @@ def buy():
                     updated_total = existing_total_int + int(total)  # Convert shares to an integer
                     
                     db.execute("UPDATE purchases SET shares = ?, total = ? WHERE stock = ? AND id = ?", updated_shares, updated_total, stock, session["user_id"])
-                    db.execute("INSERT INTO history (id, stock, shares, price, total, type, dt) VALUES (?, ?, ?, ?, ?, 'buy', ?)", session["user_id"], stock, int(shares), price, total, current_datetime)
+                    db.execute("INSERT INTO history (id, stock, shares, price, total, type, dt) VALUES (?, ?, ?, ?, ?, 'BUY', ?)", session["user_id"], stock, int(shares), price, total, current_datetime)
                     db.execute("UPDATE users SET cash = ?", (cash - updated_total))
                 else:
                     db.execute("INSERT INTO purchases (id, stock, shares, price, total) VALUES (?, ?, ?, ?, ?)", session["user_id"], stock, int(shares), price, total)
