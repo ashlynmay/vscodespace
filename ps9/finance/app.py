45bb58ebc897097d7c4c581a9c32cea6f0b3b2e1
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 08:11:55 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -223,7 +223,7 @@ def sell():
         elif lookup(request.form.get("symbol")) == None:
             return apology("stock not found", 402)
         
-        db.execute
+        sharedb.execute
         if request.form.get("shares") < 1:
             return apology("you cant sell less than 1 share")
         elif request.form.get("shares") >  
