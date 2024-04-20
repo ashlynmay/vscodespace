223af791630ab53481e00a074f93cbcaf31dbdc5
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 02:40:57 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -130,7 +130,7 @@ def register():
             hpswd = generate_password_hash(request.form.get("password"))
             db.execute("INSERT INTO users (username, hash, cash) VALUES (?, ?, 0)", request.form.get("username"), hpswd)
             return redirect("/")
-        elif len(db.execute("SELECT username FROM users WHERE username IS ?" request.form.get("username") )) > 0:
+        elif len(db.execute("SELECT username FROM users WHERE username IS ?" (request.form.get("username") )) > 0:
             return apology("username already taken", 403)
 
     # User reached route via GET (as by clicking a link or via redirect)
