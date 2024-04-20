29b7bd49a894dd2d444d545e68add25b23cd2a76
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 02:08:16 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -113,7 +113,7 @@ def quote():
 def register():
     """Register user"""
     rows = db.execute(
-            "INSERT name, FROM users WHERE username = ?", request.form.get("username")
+            "INSERT username  FROM users WHERE username = ?", request.form.get("username")
         )
 
         # Ensure username exists and password is correct
