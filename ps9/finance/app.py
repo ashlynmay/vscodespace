54ea1affb3390a1cc2ec94525de5efd0aeb86ac6
ipsum <may.ashlyn0723@gmail.com>
2024-04-20 02:10:07 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -117,7 +117,7 @@ def register():
         )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
+     if len(rows) != 1 or not check_password_hash(
             rows[0]["hash"], request.form.get("password")
         ):
             return apology("invalid username and/or password", 403)
