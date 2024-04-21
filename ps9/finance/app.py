261307ddbc0119a76d47cb8b16788b3dc99f6e49
ipsum <may.ashlyn0723@gmail.com>
2024-04-21 01:04:50 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -261,6 +261,7 @@ def sell():
         price = lookup(stock)["price"]
         ntotal = int(request.form.get("shares")) * price
         updated_total = total - ntotal
+        newcash = total +
         current_datetime = datetime.now()
         if int(request.form.get("shares")) > shares:
             return apology("you dont have that many shares to sell", 402)
