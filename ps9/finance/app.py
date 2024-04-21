ccc77d00fccf6cc96fca4bb0b3806af3a2e6482b
ipsum <may.ashlyn0723@gmail.com>
2024-04-21 01:06:39 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -261,7 +261,7 @@ def sell():
         price = lookup(stock)["price"]
         ntotal = float(request.form.get("shares")) * price
         updated_total = total - ntotal
-        cash = db.execute("SELECT cash FROM ")
+        cash = db.execute("SELECT cash FROM users WHERE id = ?")
         newcash = cash + ntotal
         current_datetime = datetime.now()
         if int(request.form.get("shares")) > shares:
