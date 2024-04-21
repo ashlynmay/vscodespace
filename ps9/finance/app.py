8c465bc7614a77831e60c655fc4cb16fb6522340
ipsum <may.ashlyn0723@gmail.com>
2024-04-21 02:22:23 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -261,6 +261,7 @@ def sell():
     if request.method == "POST":
         stock = request.form.get("symbol")
         stock = stock.upper()
+        stocks = db.execute("SELECT stock FROM payments WHERE id = ?", session["user_id"])
         if not request.form.get("symbol") or not request.form.get("shares"):
             return apology("must provide stock symbol and the amount of shares", 402)
         elif lookup(stock) == None:
