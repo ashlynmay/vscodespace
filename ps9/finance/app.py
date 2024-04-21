79d518e332411eac790f700eaebfac314c9adab2
ipsum <may.ashlyn0723@gmail.com>
2024-04-21 00:56:27 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -33,7 +33,7 @@ def after_request(response):
 @app.route("/cash", methods=["GET", "POST"])
 @login_required
 def index():
-    balance = db.execute("SELECT ")
+    balance = db.execute("SELECT cash FROM ")
     if request.method == "POST":
         
     else:
