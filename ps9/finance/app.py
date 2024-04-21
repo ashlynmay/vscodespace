fca740259350d3ca82e443d60fe9c1457e05075e
ipsum <may.ashlyn0723@gmail.com>
2024-04-21 01:40:43 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -83,7 +83,7 @@ def buy():
             return apology("stock not found", 400)
         elif int(request.form.get("shares")) < 1:
             return apology("you cannot purchase less than 1 share", 400)
-        elif YourNumber.toString.Contains(“.”)request.form.get("shares") 
+        elif request.form.get("shares").toString.Contains(“.”) 
         else:
             cash_result = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
             cash = cash_result[0]["cash"] if cash_result else 0
