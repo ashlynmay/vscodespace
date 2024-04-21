2b1eaad450484e5ed6126786ec75ee5093786b9b
ipsum <may.ashlyn0723@gmail.com>
2024-04-21 00:59:06 +0000

    Invalid DateTime

--- ps9/finance/app.py
+++ ps9/finance/app.py
@@ -36,7 +36,7 @@ def index():
     get_balance = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
     balance = get_balance[0]["cash"]
     if request.method == "POST":
-        request.form.get("cash")
+        addcash = request.form.get("cash")
     else:
         return render_template("cash.html", balance=balance)
 
