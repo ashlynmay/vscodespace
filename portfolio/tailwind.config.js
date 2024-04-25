e7e08850e7281f4b9adda5ce047021e151f619e3
ipsum <may.ashlyn0723@gmail.com>
2024-04-25 21:08:20 +0000

    Invalid DateTime

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: 
-  ["./templates/**/*.html",
-  "./static/src/**/*.js"],
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 }
-
