7ff4e03d919d3d90cfb6a3de3cfb04bf9e24fcb2
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 18:13:22 +0000

    Invalid DateTime

--- portfolio/static/src/main.js
+++ portfolio/static/src/main.js
@@ -1,66 +1,65 @@
-import EmblaCarousel from 'embla-carousel'
-import { addPrevNextBtnsClickHandlers } from './EmblaCarouselArrowButtons'
-import '../css/base.css'
-import '../css/sandbox.css'
-import '../css/embla.css'
+import EmblaCarousel from "embla-carousel";
+import { addPrevNextBtnsClickHandlers } from "./EmblaCarouselArrowButtons";
+import "../css/base.css";
+import "../css/sandbox.css";
+import "../css/embla.css";
 
-const OPTIONS = { align: 'start', loop: true }
+const OPTIONS = { align: "start", loop: true };
 
-const emblaNode = document.querySelector('.embla')
-const viewportNode = emblaNode.querySelector('.embla__viewport')
-const prevBtnNode = emblaNode.querySelector('.embla__button--prev')
-const nextBtnNode = emblaNode.querySelector('.embla__button--next')
+const emblaNode = document.querySelector(".embla");
+const viewportNode = emblaNode.querySelector(".embla__viewport");
+const prevBtnNode = emblaNode.querySelector(".embla__button--prev");
+const nextBtnNode = emblaNode.querySelector(".embla__button--next");
 
-const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS);
 
 const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
   emblaApi,
   prevBtnNode,
   nextBtnNode
-)
+);
 
-emblaApi.on('destroy', removePrevNextBtnsClickHandlers)
+emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
 
 const addTogglePrevNextBtnsActive = (emblaApi, prevBtn, nextBtn) => {
-    const togglePrevNextBtnsState = () => {
-      if (emblaApi.canScrollPrev()) prevBtn.removeAttribute('disabled')
-      else prevBtn.setAttribute('disabled', 'disabled')
-  
-      if (emblaApi.canScrollNext()) nextBtn.removeAttribute('disabled')
-      else nextBtn.setAttribute('disabled', 'disabled')
-    }
-  
-    emblaApi
-      .on('select', togglePrevNextBtnsState)
-      .on('init', togglePrevNextBtnsState)
-      .on('reInit', togglePrevNextBtnsState)
-  
-    return () => {
-      prevBtn.removeAttribute('disabled')
-      nextBtn.removeAttribute('disabled')
-    }
-  }
-  
-  export const addPrevNextBtnsClickHandlers = (emblaApi, prevBtn, nextBtn) => {
-    const scrollPrev = () => {
-      emblaApi.scrollPrev()
-    }
-    const scrollNext = () => {
-      emblaApi.scrollNext()
-    }
-    prevBtn.addEventListener('click', scrollPrev, false)
-    nextBtn.addEventListener('click', scrollNext, false)
-  
-    const removeTogglePrevNextBtnsActive = addTogglePrevNextBtnsActive(
-      emblaApi,
-      prevBtn,
-      nextBtn
-    )
-  
-    return () => {
-      removeTogglePrevNextBtnsActive()
-      prevBtn.removeEventListener('click', scrollPrev, false)
-      nextBtn.removeEventListener('click', scrollNext, false)
-    }
-  }
-  
+  const togglePrevNextBtnsState = () => {
+    if (emblaApi.canScrollPrev()) prevBtn.removeAttribute("disabled");
+    else prevBtn.setAttribute("disabled", "disabled");
+
+    if (emblaApi.canScrollNext()) nextBtn.removeAttribute("disabled");
+    else nextBtn.setAttribute("disabled", "disabled");
+  };
+
+  emblaApi
+    .on("select", togglePrevNextBtnsState)
+    .on("init", togglePrevNextBtnsState)
+    .on("reInit", togglePrevNextBtnsState);
+
+  return () => {
+    prevBtn.removeAttribute("disabled");
+    nextBtn.removeAttribute("disabled");
+  };
+};
+
+export const addPrevNextBtnsClickHandlers = (emblaApi, prevBtn, nextBtn) => {
+  const scrollPrev = () => {
+    emblaApi.scrollPrev();
+  };
+  const scrollNext = () => {
+    emblaApi.scrollNext();
+  };
+  prevBtn.addEventListener("click", scrollPrev, false);
+  nextBtn.addEventListener("click", scrollNext, false);
+
+  const removeTogglePrevNextBtnsActive = addTogglePrevNextBtnsActive(
+    emblaApi,
+    prevBtn,
+    nextBtn
+  );
+
+  return () => {
+    removeTogglePrevNextBtnsActive();
+    prevBtn.removeEventListener("click", scrollPrev, false);
+    nextBtn.removeEventListener("click", scrollNext, false);
+  };
+};
