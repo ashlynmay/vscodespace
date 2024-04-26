5f634650c386490b1303c32226820344785cf34f
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 16:26:33 +0000

    Invalid DateTime

--- portfolio/app.py
+++ portfolio/app.py
@@ -40,27 +40,58 @@ def past_projects():
                 for filename in filenames:
                     if filename.lower() == 'readme.md':
                         # Read the content of the README file
-                        with open(os.path.join(dirpath, filename), 'r') as file:
-                            content = file.read()
-                            content = re.sub(r"<!--(.*?)-->", r"\1", content, flags=re.DOTALL)
-                            lines = content.splitlines()
-                            if len(lines) >= 1:
-                                name = lines[0].strip()
-                            else: name = ""
-                            if len(lines) >- 2:    
-                                image = lines[1].strip()
-                            else: image = ""
-                            if len(lines) >= 3:
-                                description = lines[2].strip()
-                            else:
-                                description = ""
-                            if len(lines) >= 4:
-                                link = lines[3].strip()
-                            else:
-                                link = ""
++                        try:
++                            with open(os.path.join(dirpath, filename), 'r') as file:
++                                content = file.read()
++                        except FileNotFoundError:
++                            # File may have moved or been deleted
++                            continue
++                        content = re.sub(r"<!--(.*?)-->", r"\1", content, flags=re.DOTALL)
++                        lines = content.splitlines()
++                        try:
++                            name = lines[0].strip()
++                        except IndexError:
++                            name = ""
++                        try:
++                            image = lines[1].strip()
++                        except IndexError:
++                            image = ""
++                        try:
++                            description = lines[2].strip()
++                        except IndexError:
++                            description = ""
++                        try:
++                            link = lines[3].strip()
++                        except IndexError:
++                            link = ""
++                        try:
+-                        with open(os.path.join(dirpath, filename), 'r') as file:
+-                            content = file.read()
+-                            content = re.sub(r"<!--(.*?)-->", r"\1", content, flags=re.DOTALL)
+-                            lines = content.splitlines()
+-                            if len(lines) >= 1:
+-                                name = lines[0].strip()
+-                            else: name = ""
+-                            if len(lines) >- 2:    
+-                                image = lines[1].strip()
+-                            else: image = ""
+-                            if len(lines) >= 3:
+-                                description = lines[2].strip()
+-                            else:
+-                                description = ""
+-                            if len(lines) >= 4:
+-                                link = lines[3].strip()
+-                            else:
+-                                link = ""
                             db.execute("INSERT INTO projects (name, image, description, link) VALUES (?, ?, ?, ?)", name, image, description, link)
++                        except:
++                            pass
         continue
-    projects = db.execute("SELECT * FROM projects")
++    try:
++        projects = db.execute("SELECT * FROM projects")
++    except:
++        projects = None
+-    projects = db.execute("SELECT * FROM projects")
     return render_template("past-projects.html", projects=projects)
 <<<<<<<  2a968b78-86e4-4d96-a594-5f041024d49f  >>>>>>>
 
