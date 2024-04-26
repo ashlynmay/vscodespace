eae1f3e7c594e96b1f630653a273b45be1b3fcd7
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 15:02:52 +0000

    Invalid DateTime

--- portfolio/app.py
+++ portfolio/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/portfolio")
+@app.route("/current-projects")
 def portfolio():
     
     return render_template("portfolio.html")
