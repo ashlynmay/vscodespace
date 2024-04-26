f62c5f9d0832269f7150c3d5d09b456fff404538
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 18:12:53 +0000

    Invalid DateTime

--- portfolio/static/src/main.js
+++ portfolio/static/src/main.js
@@ -1,7 +1,22 @@
 import EmblaCarousel from 'embla-carousel'
+import { addPrevNextBtnsClickHandlers } from './EmblaCarouselArrowButtons'
+import '../css/base.css'
+import '../css/sandbox.css'
+import '../css/embla.css'
+
+const OPTIONS = { align: 'start', loop: true }
 
 const emblaNode = document.querySelector('.embla')
-const options = { loop: false }
-const emblaApi = EmblaCarousel(emblaNode, options)
+const viewportNode = emblaNode.querySelector('.embla__viewport')
+const prevBtnNode = emblaNode.querySelector('.embla__button--prev')
+const nextBtnNode = emblaNode.querySelector('.embla__button--next')
 
-console.log(emblaApi.slideNodes()) // Access API
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
+
+const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+  emblaApi,
+  prevBtnNode,
+  nextBtnNode
+)
+
+emblaApi.on('destroy', removePrevNextBtnsClickHandlers)
