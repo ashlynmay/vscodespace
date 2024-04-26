7a441b14a3fc69c9a85855e5995b7a318e2ee209
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 16:51:23 +0000

    Invalid DateTime

--- portfolio/app.py
+++ portfolio/app.py
@@ -42,6 +42,10 @@ def past_projects():
                     link = lines[3].strip()
                 else:
                     link = ""
+                if len(lines) >= 4:
+                    link = lines[3].strip()
+                else:
+                    link = ""
                 db.execute("INSERT INTO projects (name, image, description, link, type) VALUES (?, ?, ?, ?)", name, image, description, link, type)
     projects = db.execute("SELECT * FROM projects")
     return render_template("past-projects.html", projects=projects)
