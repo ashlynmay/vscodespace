4c81ff0c3c19c681ea8223f987460435a4c2ec7b
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 16:13:52 +0000

    Invalid DateTime

--- portfolio/app.py
+++ portfolio/app.py
@@ -23,31 +23,56 @@ def past_projects():
     # Directory path to search for README files
     directory = '/home/ipsum/vscodespace/'
 
-    # Iterate over files in the directory
-    for filename in os.listdir(directory):
-        if filename.lower() == 'readme.md':
-            # Read the content of the README file
-            with open(os.path.join(directory, filename), 'r') as file:
-                content = file.read()
-                content = re.sub(r"<!--(.*?)-->", r"\1", content, flags=re.DOTALL)
-                lines = content.splitlines()
-                if len(lines) >= 1:
-                    name = lines[0].strip()
-                else: name = ""
-                if len(lines) >- 2:    
-                    image = lines[1].strip()
-                else: image = ""
-                if len(lines) >= 3:
-                    description = lines[2].strip()
-                else:
-                    description = ""
-                if len(lines) >= 4:
-                    link = lines[3].strip()
-                else:
-                    link = ""
-                db.execute("INSERT INTO projects (name, image, description, link) VALUES (?, ?, ?, ?)", name, image, description, link)
++    # Iterate over files in the directory and child dirs
++    for dirpath, dirnames, filenames in os.walk(directory):
++        for filename in filenames:
++            if filename.lower() == 'readme.md':
++                # Read the content of the README file
++                with open(os.path.join(dirpath, filename), 'r') as file:
++                    content = file.read()
++                    content = re.sub(r"<!--(.*?)-->", r"\1", content, flags=re.DOTALL)
++                    lines = content.splitlines()
++                    if len(lines) >= 1:
++                        name = lines[0].strip()
++                    else: name = ""
++                    if len(lines) >- 2:    
++                        image = lines[1].strip()
++                    else: image = ""
++                    if len(lines) >= 3:
++                        description = lines[2].strip()
++                    else:
++                        description = ""
++                    if len(lines) >= 4:
++                        link = lines[3].strip()
++                    else:
++                        link = ""
++                    db.execute("INSERT INTO projects (name, image, description, link) VALUES (?, ?, ?, ?)", name, image, description, link)
+-    # Iterate over files in the directory
+-    for filename in os.listdir(directory):
+-        if filename.lower() == 'readme.md':
+-            # Read the content of the README file
+-            with open(os.path.join(directory, filename), 'r') as file:
+-                content = file.read()
+-                content = re.sub(r"<!--(.*?)-->", r"\1", content, flags=re.DOTALL)
+-                lines = content.splitlines()
+-                if len(lines) >= 1:
+-                    name = lines[0].strip()
+-                else: name = ""
+-                if len(lines) >- 2:    
+-                    image = lines[1].strip()
+-                else: image = ""
+-                if len(lines) >= 3:
+-                    description = lines[2].strip()
+-                else:
+-                    description = ""
+-                if len(lines) >= 4:
+-                    link = lines[3].strip()
+-                else:
+-                    link = ""
+-                db.execute("INSERT INTO projects (name, image, description, link) VALUES (?, ?, ?, ?)", name, image, description, link)
     projects = db.execute("SELECT * FROM projects")
     return render_template("past-projects.html", projects=projects)
++
 <<<<<<<  0ec327a0-b508-465e-8ed7-6b30e462002f  >>>>>>>
 
 if __name__ == '__main__':
