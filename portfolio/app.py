1d18f3d16fc3640030044cbef3b947a4f0636f9a
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 15:58:19 +0000

    Invalid DateTime

--- portfolio/app.py
+++ portfolio/app.py
@@ -26,7 +26,7 @@ def past_projects():
         if filename.lower() == 'readme.md':
             # Read the content of the README file
             with open(os.path.join(directory, filename), 'r') as file:
-                
+                files1=re.sub("[$@&]","",s)
                 lines = file.readlines()
                 if len(lines) >= 1:
                     name = lines[0].strip()
