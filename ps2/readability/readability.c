7749544b145281a42aa60a6fb2b2aaaa20793c28
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 20:13:24 +0000

    Invalid DateTime

--- ps2/readability/readability.c
+++ ps2/readability/readability.c
@@ -101,8 +101,7 @@ $ ./readability
     Grade 3                                                                         
 $ ./readability                                                                 
     Text: Harry Potter was a highly unusual boy in many ways. 
-    For one thing, he hate
-    d the summer holidays more than any other time of year. 
+    For one thing, he hated the summer holidays more than any other time of year. 
     For another, he really wanted to do his homework, but 
     was forced to do it in secret, in the dead of the 
     night. And he also happened to be a wizard.
