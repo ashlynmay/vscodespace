f05ec5c3553baf151fb24b393f8c765d779f84c2
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 18:49:32 +0000

    Invalid DateTime

--- portfolio/static/src/main.js
+++ portfolio/static/src/main.js
@@ -1,65 +1,31 @@
-import EmblaCarousel from "embla-carousel";
-import { addPrevNextBtnsClickHandlers } from "./EmblaCarouselArrowButtons";
-import "../css/base.css";
-import "../css/sandbox.css";
-import "../css/embla.css";
+import EmblaCarousel from 'embla-carousel'
+import ClassNames from 'embla-carousel-class-names'
+import { addDotBtnsAndClickHandlers } from './EmblaCarouselDotButton'
+import { addPrevNextBtnsClickHandlers } from './EmblaCarouselArrowButtons'
+import '../css/base.css'
+import '../css/sandbox.css'
+import '../css/embla.css'
 
-const OPTIONS = { align: "start", loop: true };
+const OPTIONS = {}
 
-const emblaNode = document.querySelector(".embla");
-const viewportNode = emblaNode.querySelector(".embla__viewport");
-const prevBtnNode = emblaNode.querySelector(".embla__button--prev");
-const nextBtnNode = emblaNode.querySelector(".embla__button--next");
+const emblaNode = document.querySelector('.embla')
+const viewportNode = emblaNode.querySelector('.embla__viewport')
+const prevBtn = emblaNode.querySelector('.embla__button--prev')
+const nextBtn = emblaNode.querySelector('.embla__button--next')
+const dotsNode = document.querySelector('.embla__dots')
 
-const emblaApi = EmblaCarousel(viewportNode, OPTIONS);
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [ClassNames()])
 
 const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
   emblaApi,
-  prevBtnNode,
-  nextBtnNode
-);
+  prevBtn,
+  nextBtn
+)
+const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+  emblaApi,
+  dotsNode
+)
 
-emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
-
-const addTogglePrevNextBtnsActive = (emblaApi, prevBtn, nextBtn) => {
-  const togglePrevNextBtnsState = () => {
-    if (emblaApi.canScrollPrev()) prevBtn.removeAttribute("disabled");
-    else prevBtn.setAttribute("disabled", "disabled");
-
-    if (emblaApi.canScrollNext()) nextBtn.removeAttribute("disabled");
-    else nextBtn.setAttribute("disabled", "disabled");
-  };
-
-  emblaApi
-    .on("select", togglePrevNextBtnsState)
-    .on("init", togglePrevNextBtnsState)
-    .on("reInit", togglePrevNextBtnsState);
-
-  return () => {
-    prevBtn.removeAttribute("disabled");
-    nextBtn.removeAttribute("disabled");
-  };
-};
-
-export const addPrevNextBtnsClickHandlers = (emblaApi, prevBtn, nextBtn) => {
-  const scrollPrev = () => {
-    emblaApi.scrollPrev();
-  };
-  const scrollNext = () => {
-    emblaApi.scrollNext();
-  };
-  prevBtn.addEventListener("click", scrollPrev, false);
-  nextBtn.addEventListener("click", scrollNext, false);
-
-  const removeTogglePrevNextBtnsActive = addTogglePrevNextBtnsActive(
-    emblaApi,
-    prevBtn,
-    nextBtn
-  );
-
-  return () => {
-    removeTogglePrevNextBtnsActive();
-    prevBtn.removeEventListener("click", scrollPrev, false);
-    nextBtn.removeEventListener("click", scrollNext, false);
-  };
-};
+emblaApi
+  .on('destroy', removePrevNextBtnsClickHandlers)
+  .on('destroy', removeDotBtnsAndClickHandlers)
