ac1c6832c4b5bd443a73192c2d4a8c8490c7d5cb
ipsum <may.ashlyn0723@gmail.com>
2024-04-26 16:24:25 +0000

    Invalid DateTime

--- portfolio/app.py
+++ portfolio/app.py
@@ -29,28 +29,7 @@ def past_projects():
                 # Include files in the same directory as 'node_modules'
                 # Process the files here
             continue
-        for filename in filenames:
-            if filename.lower() == 'readme.md':
-                # Read the content of the README file
-                with open(os.path.join(dirpath, filename), 'r') as file:
-                    content = file.read()
-                    content = re.sub(r"<!--(.*?)-->", r"\1", content, flags=re.DOTALL)
-                    lines = content.splitlines()
-                    if len(lines) >= 1:
-                        name = lines[0].strip()
-                    else: name = ""
-                    if len(lines) >- 2:    
-                        image = lines[1].strip()
-                    else: image = ""
-                    if len(lines) >= 3:
-                        description = lines[2].strip()
-                    else:
-                        description = ""
-                    if len(lines) >= 4:
-                        link = lines[3].strip()
-                    else:
-                        link = ""
-                    db.execute("INSERT INTO projects (name, image, description, link) VALUES (?, ?, ?, ?)", name, image, description, link)
+        
     projects = db.execute("SELECT * FROM projects")
     return render_template("past-projects.html", projects=projects)
 
