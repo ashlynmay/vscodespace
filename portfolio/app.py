aa7eb56c1ea798de893a6dc05f9e5a177fa3d0d2
ipsum <may.ashlyn0723@gmail.com>
2024-04-27 22:36:12 +0000

    April 27, 2024 at 5:36 PM CDT

--- portfolio/app.py
+++ portfolio/app.py
@@ -50,7 +50,7 @@ def projects():
                 db.execute("INSERT INTO projects (name, image, description, link, type, featured) VALUES (?, ?, ?, ?, ?, ?)", name, image, description, link, type, featured)
     featured_c_projects = db.execute("SELECT * FROM projects WHERE type = 'c'")
     featured_py_projects = db.execute("SELECT * FROM projects WHERE type = 'python'")
-    web_projects = db.execute("SELECT * FROM projects WHERE type = 'web'")
+    featured_web_projects = db.execute("SELECT * FROM projects WHERE type = 'web'")
     other_projects = db.execute("SELECT * FROM projects WHERE type = 'other'")
     return render_template("projects.html", c_projects=c_projects, py_projects=py_projects, web_projects=web_projects, other_projects=other_projects)
 
