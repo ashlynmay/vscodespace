5e1fe406fbe41844af5aecc2b6b85f40d2fc5909
ipsum <may.ashlyn0723@gmail.com>
2024-04-27 22:34:43 +0000

    April 27, 2024 at 5:34 PM CDT

--- portfolio/app.py
+++ portfolio/app.py
@@ -46,7 +46,7 @@ def projects():
                 if len(lines) >= 6:
                     featured = lines[5].strip()
                 else:
-                    type = ""
+                    featured = ""
                 db.execute("INSERT INTO projects (name, image, description, link, type) VALUES (?, ?, ?, ?, ?)", name, image, description, link, type)
     c_projects = db.execute("SELECT * FROM projects WHERE type = 'c'")
     py_projects = db.execute("SELECT * FROM projects WHERE type = 'python'")
