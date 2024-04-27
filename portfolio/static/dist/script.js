18d8c93949847818650f8da17e859a2ccf0a9087
ipsum <may.ashlyn0723@gmail.com>
2024-04-27 04:17:44 +0000

    April 26, 2024 at 11:17 PM CDT

--- portfolio/static/dist/script.js
+++ portfolio/static/dist/script.js
@@ -100,6 +100,6 @@ function navigation(slider) {
   
   var slider = new KeenSlider(
     "#my-keen-slider",
-    { slides: { perView: 3, spacing: 15 }, },
+    { mode: 'free', slides: { perView: 3, spacing: 15 }, },
     [navigation]
   );
