bf66e34a336bf39256fb844e2bf91c9241face75
ipsum <may.ashlyn0723@gmail.com>
2024-04-28 06:16:12 +0000

    April 28, 2024 at 1:16 AM CDT

--- portfolio/app.py
+++ portfolio/app.py
@@ -105,7 +105,7 @@ def other_projects():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("404.html")
 
 if __name__ == '__main__':
     server = Server(app.wsgi_app)
