650ec708f49f66db774baae68c90f9b2d5d0ae76
ipsum <may.ashlyn0723@gmail.com>
2024-04-28 03:53:56 +0000

    April 27, 2024 at 10:53 PM CDT

--- portfolio/app.py
+++ portfolio/app.py
@@ -63,7 +63,43 @@ def cali():
 
 @app.route("/c-projects")
 def c_projects():
-        
+    db.execute("DROP TABLE projects")
+    db.execute("CREATE TABLE projects (id INTEGER PRIMARY KEY, name TEXT, description TEXT, image TEXT, link TEXT, type TEXT, featured BOOLEAN)")
+
+    # Iterate over files in the directory
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.lower() == 'project.md':
+                with open(os.path.join(root, filename), 'r') as file:
+                    lines = file.read().splitlines()
+                if len(lines) >= 1:
+                    name = lines[0].strip()
+                else: name = ""
+                if len(lines) > 1:    
+                    image = lines[1].strip()
+                else: image = ""
+                if len(lines) >= 3:
+                    description = lines[2].strip()
+                else:
+                    description = ""
+                if len(lines) >= 4:
+                    link = lines[3].strip()
+                else:
+                    link = ""
+                if len(lines) >= 5:
+                    type = lines[4].strip()
+                else:
+                    type = ""
+                if len(lines) >= 6:
+                    featured = lines[5].strip()
+                else:
+                    featured = "0"
+                db.execute("INSERT INTO projects (name, image, description, link, type, featured) VALUES (?, ?, ?, ?, ?, ?)", name, image, description, link, type, featured)
+    featured_c_projects = db.execute("SELECT * FROM projects WHERE type = 'c' AND featured = '1'")
+    featured_py_projects = db.execute("SELECT * FROM projects WHERE type = 'python' AND featured = '1'")
+    featured_web_projects = db.execute("SELECT * FROM projects WHERE type = 'web' AND featured = '1'")
+    featured_other_projects = db.execute("SELECT * FROM projects WHERE type = 'other' AND featured = '1'")
+    return render_template("projects.html", c_projects=featured_c_projects, py_projects=featured_py_projects, web_projects=featured_web_projects, other_projects=featured_other_projects)
         
 
 
