e46bacb19b38bd47624ed933ef7a8c7043f4f7a5
ipsum <may.ashlyn0723@gmail.com>
2024-04-28 04:24:00 +0000

    April 27, 2024 at 11:24 PM CDT

--- portfolio/app.py
+++ portfolio/app.py
@@ -66,39 +66,7 @@ def projects():
     db.execute("DROP TABLE projects")
     db.execute("CREATE TABLE projects (id INTEGER PRIMARY KEY, name TEXT, description TEXT, image TEXT, link TEXT, type TEXT, featured BOOLEAN, alt TEXT)")
 
-    # Iterate over files in the directory
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
-            if filename.lower() == 'project.md':
-                with open(os.path.join(root, filename), 'r') as file:
-                    lines = file.read().splitlines()
-                if len(lines) >= 1:
-                    name = lines[0].strip()
-                else: name = ""
-                if len(lines) > 1:    
-                    image = lines[1].strip()
-                else: image = ""
-                if len(lines) >= 3:
-                    description = lines[2].strip()
-                else:
-                    description = ""
-                if len(lines) >= 4:
-                    link = lines[3].strip()
-                else:
-                    link = ""
-                if len(lines) >= 5:
-                    type = lines[4].strip()
-                else:
-                    type = ""
-                if len(lines) >= 6:
-                    featured = lines[5].strip()
-                else:
-                    featured = "0"
-                if len(lines) >= 7:
-                    alt = lines[5].strip()
-                else:
-                    alt = "unknown"
-                db.execute("INSERT INTO projects (name, image, description, link, type, featured, alt) VALUES (?, ?, ?, ?, ?, ?, ?)", name, image, description, link, type, featured, alt)
+    project_check()
     featured_c_projects = db.execute("SELECT * FROM projects WHERE type = 'c' AND featured = '1'")
     featured_py_projects = db.execute("SELECT * FROM projects WHERE type = 'python' AND featured = '1'")
     featured_web_projects = db.execute("SELECT * FROM projects WHERE type = 'web' AND featured = '1'")
