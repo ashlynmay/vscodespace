b71d391d63b2f4b099a4d24e0fd48a0b801eaf98
ipsum <may.ashlyn0723@gmail.com>
2024-04-28 02:07:51 +0000

    April 27, 2024 at 9:07 PM CDT

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "forest", "retro"],
+    themes: ["light", "dark", "forest", "pastel"],
   },
 }
