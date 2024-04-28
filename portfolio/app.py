8c576366abdaae9bbb5e6bf9f3307966fe5037a2
ipsum <may.ashlyn0723@gmail.com>
2024-04-28 03:51:19 +0000

    April 27, 2024 at 10:51 PM CDT

--- portfolio/app.py
+++ portfolio/app.py
@@ -105,7 +105,6 @@ def c_projects():
                     )
         projects = db.execute("SELECT * FROM projects WHERE type = 'c'")
         return render_template("c-projects.html", projects=projects)
-    else:
         projects = db.execute("SELECT * FROM projects WHERE type = 'c'")
         return render_template("c-projects.html", projects=projects)
         
